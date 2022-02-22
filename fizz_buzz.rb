4c63317c35c089e45b27072242a2583c294efb44
Yuka <yuaaasly116@icloud.com>
2022-02-22 02:57:23 +0000

    配列

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,20 +1,18 @@
 def fizz_buzz(number)
-  if number % 15 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
+  if number % 3 == 0
+    puts "Fizz"
   elsif number % 5 == 0
-    "Buzz"
-  else 
+    puts "Buzz"
+  elsif number % 15 == 0
+    puts "FizzBuzz"
+  else
     number.to_s
   end
 end
 
-puts "数字を入力してください"
+puts "数値を入力してください"
 
 input = gets.to_i
 
 puts "結果は"
-
 puts fizz_buzz(input)
-
